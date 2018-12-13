3021780d55e335bad23ea36466ac32089108fbb0
NVKPAVANKUMAR <npavankumar144@gmail.com>
2018-12-13 17:03:30 +0530

    log file added

--- Locators/locators.py
+++ Locators/locators.py
@@ -6,12 +6,3 @@ class Locators:
 
     # Home page objects
     logout_button = '//*[@id="cms-menu"]/div[1]/div[2]/a[2]'
-
-    # Moodle_sandbox Login page objects
-    usn_textbox_id = 'username'
-    pwd_textbox_id = 'password'
-    signIn_button_id = 'loginbtn'
-
-    # Moodle_sandbox Home page objects
-    admin_user_dropdown = 'dropdown-1'
-    logout_option = '#actionmenuaction-6'
--- testsuite.py
+++ testsuite.py
@@ -1,9 +1,32 @@
 import unittest
-import HtmlTestRunner
 from scenarios.scenario_a import TestSceanarioA
 
 scenario_a = unittest.TestLoader().loadTestsFromTestCase(TestSceanarioA)
 test_suite = unittest.TestSuite([scenario_a])
-# initialize a runner, pass it your suite and run it
-runner = HtmlTestRunner.HTMLTestRunner(output='test-reports', verbosity=2)
-runner.run(test_suite)
+logFile = open("logfile.txt", "a")
+unittest.TextTestRunner(verbosity=2, stream=logFile).run(test_suite)
+
+# # initialize the test suite
+# loader = unittest.TestLoader()
+# suite = unittest.TestSuite()
+#
+# # add tests to the test suite
+# suite.addTests(loader.loadTestsFromTestCase(TestSceanarioA))
+#
+# # initialize a runner, pass it your suite and run it
+# runner = HtmlTestRunner.HTMLTestRunner(output='test-reports', verbosity=2, report_title="Regression Suite")
+# runner.run(suite)
+
+# suite = unittest.TestSuite()
+# suite.addTests([unittest.defaultTestLoader.loadTestsFromTestCase(TestSceanarioA)])
+# runner = HtmlTestRunner.HTMLTestRunner(output='test-reports', verbosity=3, report_title="Regression Suite")
+# runner.run(suite)
+# logger = logging.getLogger()
+# with open('report.html', 'a') as report_file:
+#     runner = HtmlTestRunner.HTMLTestRunner(output=report_file, verbosity=2, report_title="Regression Suite")
+#
+#     scenario_a = unittest.TestLoader().loadTestsFromTestCase(TestSceanarioA)
+#     test_suite = unittest.TestSuite([scenario_a])
+#
+#     result = runner.run(test_suite)
+#     print(result)
